5bd743aded2ce53d5c87f9e385e8686155b14d27
DummiesAlert <george298829@gmail.com>
2023-09-18 11:38:51 -0600

    Update

--- Fall 2023/CPSC_231/Assignment_1_Eligibility Checker for Alberta Voters/Final.py
+++ Fall 2023/CPSC_231/Assignment_1_Eligibility Checker for Alberta Voters/Final.py
@@ -6,10 +6,6 @@ month = datetime.now().month
 day = datetime.now().day
 year = datetime.now().year
 
-def quit():
-
-    print("You Are Not Eligible To Vote")
-
 print("Welcome to The Alberta Voter Program!!")
 
 cc = input("Are you a Canadian Citizen? ")
@@ -24,40 +20,122 @@ dofbYear = int(input("What is the Year of Your Birthdate? (ie, 1954) "))
 
 if cc != 'yes' and cc != 'no':
     
-    quit("Invalid Response to Citizenship")
+    print("Invalid response to citizenship.")
     
 elif ar != 'yes' and ar != 'no':
     
-    quit("Invalid Response to Residency")
+    print("Invalid response to residency.")
     
 elif dofbMon < 1 or dofbMon > 12:
     
-    quit("Invalid Month")
+    print("Invalid month.")
     
 elif dofbDay < 1 or dofbDay > 31:
     
-    quit("Invalid Day")
+    print("Invalid day.")
     
 elif dofbYear < 1900 or dofbYear > 2023:
     
-    quit("Invalid Year")
+    print("Invalid year.")
     
 else:
     
-    age = year - dofbYear
+    ageYear = year - dofbYear
+    ageMonth = month - dofbMon
+    ageDay = day - dofbDay
     
-    if age < 18:
+    if ageYear < 18 or ageMonth < month or ageDay < day:
         
-        quit("Minor, Under 18")
+        print("Invalid birth date.")
         
-    elif cc == 'No':
+    elif cc == 'no':
         
-        quit("Not a Canadian Citizen")
+        print("Not a Canadian Citizen.")
+        print("You are not eligible to vote.")
         
-    elif ar == 'No':
+    elif ar == 'no':
         
-        quit("Not an Alberta Resident")
+        print("Not an Alberta Resident.")
         
     else:
         
-        print("You are Eligible to Vote")
+        print("You are eligible to vote.")
+        
+        
+"""Problem Output:
+
+Test case 1:
+Checking the last 29 characters in output:
+===Your output:     "1954) Not a Canadian Citizen."
+===Expected output: "You are not eligible to vote."
+===You are NOT correct.
+
+Test case 2:
+Checking the last 25 characters in output:
+===Your output:     "You are eligible to vote."
+===Expected output: "You are eligible to vote."
+===You are correct.
+
+Test case 3:
+Checking the last 32 characters in output:
+===Your output:     " Invalid response to citizenship"
+===Expected output: "Invalid response to citizenship."
+===You are NOT correct.
+
+Test case 4:
+Checking the last 14 characters in output:
+===Your output:     "Invalid month."
+===Expected output: "Invalid month."
+===You are correct.
+
+Test case 5:
+Checking the last 19 characters in output:
+===Your output:     "Invalid birth date."
+===Expected output: "Invalid birth date."
+===You are correct.
+
+Test case 6:
+Checking the last 30 characters in output:
+===Your output:     "Invalid response to residency."
+===Expected output: "Invalid response to residency."
+===You are correct.
+
+Test case 7:
+Checking the last 19 characters in output:
+===Your output:     "Invalid birth date."
+===Expected output: "Invalid birth date."
+===You are correct.
+
+Test case 8:
+Checking the last 19 characters in output:
+===Your output:     "e eligible to vote."
+===Expected output: "Invalid birth date."
+===You are NOT correct.
+
+Test case 9:
+Checking the last 25 characters in output:
+===Your output:     "You are eligible to vote."
+===Expected output: "You are eligible to vote."
+===You are correct.
+
+Test case 10:
+Checking the last 25 characters in output:
+===Your output:     "You are eligible to vote."
+===Expected output: "You are eligible to vote."
+===You are correct.
+
+Test case 11:
+Checking the last 29 characters in output:
+===Your output:     "54) You are eligible to vote."
+===Expected output: "You are not eligible to vote."
+===You are NOT correct.
+
+Test case 12:
+Checking the last 12 characters in output:
+===Your output:     "Invalid day."
+===Expected output: "Invalid day."
+===You are correct.
+
+Total correct: 8
+Total errors: 0
+** Total failed test cases: 4"""
