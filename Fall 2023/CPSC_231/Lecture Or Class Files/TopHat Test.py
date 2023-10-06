617fa0dc0f36781abbd2d73ce0419c3eacbbec69
DummiesAlert <george298829@gmail.com>
2023-10-06 12:02:24 -0600

    Update

--- Fall 2023/CPSC_231/Lecture Or Class Files/TopHat Test.py
+++ Fall 2023/CPSC_231/Lecture Or Class Files/TopHat Test.py
@@ -10,3 +10,32 @@ while i >= 1:
     time.sleep(0.0000000000001)
     
     i += 1"""
+    
+userinput = str(input("Enter a word: "))
+print(userinput)
+
+while userinput != 'exit':
+ 
+    userinput = str(input("Enter a Word (type 'exit' to exit): "))
+    
+    if userinput != 'exit':
+        print(userinput)
+    
+print("goodbye")
+
+"""
+    
+Bin: 
+
+Enter a word: hello
+hello
+Enter a Word (type 'exit' to exit): how
+how
+Enter a Word (type 'exit' to exit): are
+are
+Enter a Word (type 'exit' to exit): you
+you
+Enter a Word (type 'exit' to exit): exit
+goodbye
+    
+"""
