e72abbc5d9c165ca92255afe89fe585066def33b
DummiesAlert <george298829@gmail.com>
2023-10-12 10:21:23 -0600

    Update

---
+++ Fall 2023/CPSC_231/Tutorials/Week6_Tutorial_Thur_NameSplit.py
@@ -0,0 +1,23 @@
+#for i in range(1, 11, 1):
+
+    #print(i)
+
+"""
+import time
+
+string = str(input("Enter a String: "))
+
+for i in range(0, 100, 1):
+
+    print([*string])
+    time.sleep(1)
+    continue
+"""
+    
+name = "Charls C"
+
+for i in range(0, len(name), 1):
+
+    print(name[i], i) #,i for numbers beside. 
+
+a = []
