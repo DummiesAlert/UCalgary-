14313109c1a57af8b8ef62d667ba0a88194197da
DummiesAlert <george298829@gmail.com>
2023-10-12 10:49:21 -0600

    Update

---
+++ Fall 2023/CPSC_231/Tutorials/Week 6_Tutorial_Thur_NameSplit.py
@@ -0,0 +1,57 @@
+#for i in range(1, 11, 1):
+
+    #print(i)
+
+"""
+import time
+
+string = str(input("Enter a String: "))
+
+for i in range(0, 100, 1):
+
+    print([*string])
+    time.sleep(1)
+    continue
+"""
+
+"""
+name = "Charls C"
+
+for i in range(0, len(name), 1):
+
+    print(name[i], i) #,i for numbers beside. 
+
+"""
+
+"""
+a = ['1', 2, 3, 4, 5, 6, '7', 8, 9, False]
+
+for a in range[1, len(a), 1]:
+
+    b = str(a).split()
+
+    print(b)
+
+"""
+
+"""
+a = ['1', 2, 3, 4, 5, 6, '7', 8, 9, False]
+
+for i in range(0, len(a)): 
+
+    if (isinstance(a[i], str)): 
+
+        print(a[i])
+
+"""
+num = int(input('Enter a number: '))
+
+sum = 0
+
+for i in range(0, num + 1):
+
+    if i % 2 == 0:
+
+        sum += i
+
+print(f"Sum Of ALL THE EVEN Numbers Is {sum}")
