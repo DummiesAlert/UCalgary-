7325b8be83d45b1c1e712e15886cb13ddd69fafa
DummiesAlert <george298829@gmail.com>
2023-10-12 10:44:11 -0600

    Update

--- Fall 2023/CPSC_231/Tutorials/Week6_Tutorial_Thur_NameSplit.py
+++ Fall 2023/CPSC_231/Tutorials/Week6_Tutorial_Thur_NameSplit.py
@@ -13,11 +13,42 @@ for i in range(0, 100, 1):
     time.sleep(1)
     continue
 """
-    
+
+"""
 name = "Charls C"
 
 for i in range(0, len(name), 1):
 
     print(name[i], i) #,i for numbers beside. 
 
-a = []
+"""
+
+"""
+a = ['1', 2, 3, 4, 5, 6, '7', 8, 9, False]
+
+for a in range[1, len(a), 1]:
+
+    b = str(a).split()
+
+    print(b)
+
+"""
+
+"""
+a = ['1', 2, 3, 4, 5, 6, '7', 8, 9, False]
+
+for i in range(0, len(a)): 
+
+    if (isinstance(a[i], str)): 
+
+        print(a[i])
+
+"""
+sum = 0
+
+for i in range(100): 
+
+    if i % 2 == 0:
+
+        sum = sum + i
+        print("sum =",sum)
