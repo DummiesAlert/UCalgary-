67d383c0d928ddc5e55abc028374bb2a3a4c1504
DummiesAlert <george298829@gmail.com>
2023-10-17 10:00:27 -0600

    Comments Done

--- Fall 2023/CPSC_231/Assignment2_Examining Trends of Birth Rates Data/Final.py
+++ Fall 2023/CPSC_231/Assignment2_Examining Trends of Birth Rates Data/Final.py
@@ -15,93 +15,126 @@ Sources:
 
 """
 
+#Creates List Boundaries
 Years = [] 
 birthRates = []
 
+#Prompt User for Amount of Data Points:
 dataPoints = int(input("How many data points do you have? "))
 
+#Create Variables and Bool for Looping Control
 inputYears = 0 
 infoPassed = True
-infoFailed = True
+infoFailed = True #Swappable
 
+#Conditions Checking if dataPoints Less Than, Equal to 0
 if dataPoints <= 0: 
     
     print("Must enter at least one data point.")
-
+    
+#For Loop Main Code
 for i in range(dataPoints): 
     
-    if infoFailed: 
+    #Conditional Verifies dataPoint Again If Passed
+    if infoPassed: 
         
+        #For Loop, So Create Last Year as PrevYear to Restore New Year in Loop
         prevYear = inputYears
+        
+        #Create i as a String
         i = str(i + 1)
         
+        #Prompt User for Year of dataPoint, when data point i
         inputYears = int(input(f"What is the year of data point {i}?"))
         
+        #Checks if inputYear in List Years
         if inputYears in Years: 
             
             print("Same year entered twice.")
             infoFailed = False
-            
+        
+        #If Passes Moves On
         else: 
             
+            #Condition to Check if inputYears is Less Than  0
             if inputYears <= 0: 
                 
                 print("Invalid year.")
                 infoFailed = False
-                
+            
+            #If Passes Moves On
             else: 
                 
+                #Check if prevYear is Greater Than inputYears
                 if prevYear > inputYears:
                     
                     print("Years must be entered in order. ")
                     infoFailed = False
-                    
+                
+                #If Passes Moves On
                 else: 
                     
+                    #Add inputYears to List Years
                     Years.append(inputYears)
                     
+                    #Prompt User for birth rate i 
                     inputRates = float(input(f"What is the birth rate of data point {i}?"))
                     
+                    #Check if inputRates if Greater Than 0.0 (float)
                     if inputRates < 0.0:
                         
                         print("Invalid birth rate.")
                         infoFailed = False
-                        
+                    
+                    #If Passes Moves On
                     else: 
                         
+                        #Add inputRates to List birthRates
                         birthRates.append(inputRates)
-                        infoPassed = False
-                        
+                        infoPassed = False #Swappable
+
+#Double Checks If All Data has Passed, infoFailed = True and infoPassed = False.             
 if infoFailed and not infoPassed:
     
+    #Prompt for startYear from Years List in Order
     startYear = int(input("Which year would you like to start with?"))
 
+    #Checks if startYear is in Years, if Not DNE
     if startYear not in Years: 
         
         print("The start year does not exist.")
         
+    #If Passes Moves On
     else: 
         
+        #Checks if endYear From Years List in Order
         endYear = int(input("Which year would you like to end with?"))
         
+        #Checks if endYear is in Years, if not DNE
         if endYear not in Years: 
             
             print("The end year does not exist.")
 
+        #Checks if endYear is Less Than startYear
         elif endYear < startYear: 
             
             print("End year must be after start year.")
-            
+        
+        #If Passes Moves On
         else: 
             
+            #Locates Indexes of startyear and endYears to Match Data in List
             startIndex = birthRates[Years.index(startYear)]
             endIndex = birthRates[Years.index(endYear)]
             
+            #Calculates Average Rate
             averRate = (startIndex + endIndex) / 2
 
+            #Set averRate to 2 Decimal Points and print. 
             averRate = format(averRate, ".2f")
             print(f"The average birth rate of these two years is {averRate}.")
 
+            #Conditional to Determine Upwards, Downwards and Sideways Trends. 
             if startIndex < endIndex:
                 
                 print("There is an upward trend.")
