e0e8b4658fcafe03aa13c3b80e8c38a0caa69310
DummiesAlert <george298829@gmail.com>
2023-10-25 20:20:24 -0600

    Update

--- Fall 2023/CPSC_231/Sample Exams/Exam 2/Exam2_Sample.py
+++ Fall 2023/CPSC_231/Sample Exams/Exam 2/Exam2_Sample.py
@@ -15,7 +15,29 @@
 
 #     print("Invalid.")
 
-# Question 2: 
+# Question 2: Decreasing Order
+
+numRows = int(input("Enter the Amount of Rows!!"))
+
+for i in range(0, numRows): 
+    for j in range(numRows, i , -1): 
+
+        print("* ", end = "")
+
+    print(" ")
+    
+numRows = int(input("Enter the Amount of Rows!!"))
+
+i = 0
+while i < numRows:
+    j = numRows
+    while j > i:
+        print("* ", end = "")
+        j -= 1
+    print(" ")
+    i += 1
+    
+# Question 2: Increasing 
 
 numRows = int(input("Enter the Amount of Rows!!"))
 
---
+++ test.py
@@ -0,0 +1,10 @@
+name = "John"
+friends = []
+ages = []
+attending = []
+
+friendindex = friends.index(name)
+age = int(input(""))
+agesindex= ages.index(age)
+
+person = attending.zip(friendindex + agesindex)
