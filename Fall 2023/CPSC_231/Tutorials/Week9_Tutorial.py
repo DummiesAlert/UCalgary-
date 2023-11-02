43253b146738b44dd94e808ac34173498e91d1d9
DummiesAlert <george298829@gmail.com>
2023-11-02 10:20:38 -0600

    Update

---
+++ Fall 2023/CPSC_231/Tutorials/Week9_Tutorial.py
@@ -0,0 +1,14 @@
+"""
+
+    Learn how to use split()
+    Dictionary, exsting and non-existing key values. 
+        q_and_a = \
+        {
+            "name": "I am Chat231. My nickname is Riley.",
+            "textbook": "The textbook we use is called The Python Workbook 2nd Edition.",
+            "break": "Fall break is November 13th to 17th.",
+            "deadline": "This assignment is due on November 10th.",
+            "final exam": "The final exam will be on December 18th at 5-7pm. Check D2L for details."
+        }
+
+"""
