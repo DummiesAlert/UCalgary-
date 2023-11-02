7c02558ec72864b5fd1b0476f08dd432cfd4cf85
DummiesAlert <george298829@gmail.com>
2023-11-02 10:51:11 -0600

    Update

--- Fall 2023/CPSC_231/Tutorials/Week9_Tutorial.py
+++ Fall 2023/CPSC_231/Tutorials/Week9_Tutorial.py
@@ -1,7 +1,10 @@
 """
 
     Learn how to use split()
-    Dictionary, exsting and non-existing key values. 
+    Dictionary, exsting and non-existing key values
+
+        If you want to call a value, cannot be after the colon. 
+    
         q_and_a = \
         {
             "name": "I am Chat231. My nickname is Riley.",
@@ -12,3 +15,61 @@
         }
 
 """
+
+#Example: 
+
+def student(): 
+
+    WTAAS = input("Want to Add a Student? ")
+    WTAAS = WTAAS.lower()
+
+    if WTAAS == 'yes': 
+
+        return True
+    
+    else: 
+    
+        return False
+
+def again():
+
+    again = str(input("Want to Add Another Student? (Yes to Continute, Quit to End) "))
+    again = again.lower()
+    
+    if again == 'quit': 
+
+        False
+
+    elif again != 'yes': 
+
+        print("Please Try Again!!!")
+        again = str(input("Want to Add Another Student? (Yes to Continute, Quit to End) "))
+        again = again.lower()
+
+student()
+
+count = 4
+control = False
+
+while control == True: 
+
+    name = str(input("Enter Name to Add: "))
+    year = str(input("Enter Year to Add: "))
+
+    STUDENTS = \
+    {
+        
+        "Student1": "Jose", 
+        "Year1": "2032", 
+        "Student2":"Gordon", 
+        "Year2":"2034", 
+        "Student3": "Charls", 
+        "Year3": "2038"
+        
+    }
+
+    STUDENTS[count] = {"Student"+str(count): name, "Year"+str(count): year}
+
+    again()
+
+    count += 1
